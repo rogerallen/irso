9dc2ca6fe0cc6bbf50622a278c1ad29079b1c832
Roger Allen <rallen@gmail.com>
2012-06-05 07:25:58 -0700

    piso plays

--- project.clj
+++ project.clj
@@ -1,3 +1,8 @@
 (defproject irso "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :description "Irrational Songs in Overtone"
+  :repositories {"local" ~(str (.toURI (java.io.File. "maven_repository")))}
+  :dependencies [ [org.clojure/clojure "1.3.0"]
+                  [overtone "0.7.0-LOCALSNAPSHOT"]
+                  ]
+  :plugins [ [lein-swank "1.4.4"] ]
+  )
