ed295b78936c94052fc9349034998905c4084c5a
Roger Allen <rallen@gmail.com>
2012-06-05 06:55:59 -0700

    initial commit

---
+++ src/irso/core.clj
@@ -0,0 +1,1 @@
+(ns irso.core)
---
+++ test/irso/test/core.clj
@@ -0,0 +1,6 @@
+(ns irso.test.core
+  (:use [irso.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
