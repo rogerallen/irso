99c1e9fc71b7befc2b0ef0c71625a2e1c47aebbc
Roger Allen <rallen@gmail.com>
2012-07-08 23:44:14 -0700

    go to 0.7.1, fix issue 108

--- project.clj
+++ project.clj
@@ -2,10 +2,10 @@
   :description "Irrational Songs in Overtone"
   :repositories {"local" ~(str (.toURI (java.io.File. "maven_repository")))}
   :dependencies [ [org.clojure/clojure "1.3.0"]
-                  [overtone "0.7.0"]
+                  [overtone "0.7.1"]
                   [quil "1.5.0"]
                   ]
   :plugins [ [lein-swank "1.4.4"] ]
-  :main irso.core
+  :main ^{:skip-aot true} irso.core
   ;; :repl-options {:timeout 60000} ;; timeout in ms
   )
