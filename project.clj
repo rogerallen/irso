f76fc4c2a64e66ffbb23f8765fee47fe2b2920ad
Roger Allen <rallen@gmail.com>
2013-01-13 18:07:26 -0800

    Prep for trying to debug 0.8.0-RC15(ish) isshie.  Jump back to 0.8.0-RC10 as a known-good version.  Remove oversampler dependency so that others can easily repro issue.

--- project.clj
+++ project.clj
@@ -1,9 +1,15 @@
 (defproject irso "1.0.0-SNAPSHOT"
   :description "Irrational Songs in Overtone"
   :dependencies [ [org.clojure/clojure "1.4.0"]
-                  [overtone "0.8.0-SNAPSHOT"]
+                  [overtone "0.8.0-RC10"]
+                  ;; RC10 good
+                  ;; RC11 bad
+                  ;; RC12 bad
+                  ;; RC13 window+sound, sometimes works, sometimes noise
+                  ;; RC14 no window
+                  ;; RC15 no window, sometimes music. often noise
                   [quil "1.6.0"]
-                  [oversampler "0.2.0-SNAPSHOT"]
+                  ;; [oversampler "0.2.0-SNAPSHOT"]
                   ]
   :plugins [ [lein-swank "1.4.4"] ]
   :main ^{:skip-aot true} irso.core
