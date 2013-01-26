11f98f0e6d3f89347737ea825d90e3a5d826a4f1
Roger Allen <rallen@gmail.com>
2013-01-26 15:42:28 -0800

    Update to both 0.8.0 overtone and 0.3.0 oversampler.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject irso "1.0.0-SNAPSHOT"
   :description "Irrational Songs in Overtone"
   :dependencies [ [org.clojure/clojure "1.4.0"]
-                  [overtone "0.8.0-RC20"]
+                  [overtone "0.8.0"]
                   [quil "1.6.0"]
-                  [oversampler "0.3.0-SNAPSHOT"]
+                  [oversampler "0.3.0"]
                   ]
   :plugins [ [lein-swank "1.4.4"] ]
   :main ^{:skip-aot true} irso.core
